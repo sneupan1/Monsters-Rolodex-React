db98a26841a3c788292879519b0ec83e12140285
Saurav Neupane <srvneupane@gmail.com>
2020-04-01 15:16:14 -0500

    Updates

---
+++ precache-manifest.7ca0573be132804c00b153082fbcc52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427406e3d986bdbabb9738342e0b1209",
+    "url": "/Monsters-Rolodex-React/index.html"
+  },
+  {
+    "revision": "ddd9602a43ce2953a300",
+    "url": "/Monsters-Rolodex-React/static/css/main.e441f651.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-Rolodex-React/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex-React/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd9602a43ce2953a300",
+    "url": "/Monsters-Rolodex-React/static/js/main.d81ba8fb.chunk.js"
+  },
+  {
+    "revision": "dad86f43fd5273d7fbb7",
+    "url": "/Monsters-Rolodex-React/static/js/runtime-main.834a11ce.js"
+  }
+]);
